d312f35895b6827e7e5dd19eea1f5c901775cf14
Joseph Wilk <joe@josephwilk.net>
2014-09-14 20:34:37 +0200

    ship some of my personal synths as examples

--- freedom/overtone/project.clj
+++ freedom/overtone/project.clj
@@ -5,5 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [overtone "0.9.1"]
+                 [overtone.synths "0.1.0-SNAPSHOT"] ;;Some example synths to play with
                  [shadertone "0.2.3"]
                  [mud "0.1.0-SNAPSHOT"]])
