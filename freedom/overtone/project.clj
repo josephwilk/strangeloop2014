339e2037f91a6189b6b0f45a74e4215e5d6d7332
Joseph Wilk <joe@josephwilk.net>
2014-09-16 17:45:50 -0500

    adding quill

--- freedom/overtone/project.clj
+++ freedom/overtone/project.clj
@@ -1,10 +1,11 @@
 (defproject strangeloop "0.1.0-SNAPSHOT"
   :description "Welcome to the wonderful world of SOUND"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/josephwilk/strangeloop2014"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [overtone "0.9.1"]
                  [overtone.synths "0.1.0-SNAPSHOT"] ;;Some example synths to play with
-                 [shadertone "0.2.3"]
+                 [quil "2.2.2"]                     ;;Quil for visuals OR
+                 [shadertone "0.2.3"]               ;; Shaders
                  [mud "0.1.0-SNAPSHOT"]])
